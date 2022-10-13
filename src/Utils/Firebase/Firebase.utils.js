244abed0dd79b36be833e5c64258069a7c176808
Abdulrahman Yusuf <polymathalli@gmail.com>
2022-10-13 22:43:58 +0100

    started integrating firebase

--- src/Utils/Firebase/Firebase.utils.js
+++ src/Utils/Firebase/Firebase.utils.js
@@ -1,6 +1,19 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { signInWithPopup } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// const firebaseConfig = {
-//     apiKey:
-// }
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCLkAfDRBEoFV9ko60-G41nTD5Jxk0_R3E',
+  authDomain: 'retinkassessment.firebaseapp.com',
+  projectId: 'retinkassessment',
+  storageBucket: 'retinkassessment.appspot.com',
+  messagingSenderId: '613047328767',
+  appId: '1:613047328767:web:f65526c4bd52e3d00b51ca',
+  measurementId: 'G-GYV8NFGT5V',
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
